f047053cc0aff2da4090899526b08258fb057771
Anıl <anil@Anl-MacBook-Air.local>
2023-12-21 18:55:37 +0300

    Advent calender list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+const days = [...Array(24)].map((val,index) => index+1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <FlatList 
+      data={days} 
+      contentContainerStyle={styles.content}
+      columnWrapperStyle={styles.column}
+      renderItem={({item}) => (
+        <DayListItem  day={item}/>
+      )}
+      numColumns={2}
+      />
+      
+
+   { /*  {days.map((day) => (
+        <View style={styles.box} key={day}>
+        <Text style={styles.text}>{day}</Text>
+      </View>
+      ))} */}
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +33,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
+  }
 });
