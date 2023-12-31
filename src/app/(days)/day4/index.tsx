ded0f790e5717988098192ab375cb834b49161c4
Anıl <anil@Anl-MacBook-Air.local>
2023-12-31 15:02:46 +0300

    lottie animations

---
+++ src/app/(days)/day4/index.tsx
@@ -0,0 +1,24 @@
+import MarkdownDisplay from '@/components/day3/MarkdownDisplay'
+import { Link, Stack } from 'expo-router'
+import { Button, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+const description = `
+#Markdown
+
+Animated Splash Screen
+
+`
+
+export default function DayDetailsScreen() {
+  return (
+    <SafeAreaView edges={['bottom']} style={{flex:1}}>
+    <Stack.Screen options={{title: "Day 4: SplashScreen"}}/>
+    <MarkdownDisplay>{description}</MarkdownDisplay>
+    <Link href={"/day4/animation"} asChild>
+    <Button title='Go to the animation' />
+    </Link>
+    </SafeAreaView>
+  )
+}
