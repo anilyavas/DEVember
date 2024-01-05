8401e65eecf377fcfd88104c23d76fe4839a50e0
Anıl <anil@Anl-MacBook-Air.local>
2024-01-05 14:02:53 +0300

    animations for tinder swipe

--- src/app/(days)/day6/tinder.tsx
+++ src/app/(days)/day6/tinder.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
 import TinderCard from '@/components/day6/TinderCard'
+import { Stack } from 'expo-router';
+import { useSharedValue} from "react-native-reanimated";
+
+
+const users = [
+  {
+  id: 1,
+  image: "https://via.placeholder.com/300",
+  name: "Dani"
+},
+{
+  id: 2,
+  image: "https://via.placeholder.com/300",
+  name: "Jon"
+},
+{
+  id: 3,
+  image: "https://via.placeholder.com/300",
+  name: "Alice"
+},
+{
+  id: 4,
+  image: "https://via.placeholder.com/300",
+  name: "Carl"
+},
+{
+  id: 5,
+  image: "https://via.placeholder.com/300",
+  name: "Kelsey"
+},
+];
 
 export default function TinderScreen() {
+    const activeIndex = useSharedValue(0);
   return (
-    <View>
-      <TinderCard />
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Stack.Screen options={{headerShown: false}}/>
+      {users.map((user,index) => (
+              <TinderCard key={user.id} user={user} numOfCards={users.length} index={index} activeIndex={activeIndex} />
+      ))}
     </View>
   )
 }
+const styles = StyleSheet.create({});
--- src/components/day6/TinderCard.tsx
+++ src/components/day6/TinderCard.tsx
@@ -1,20 +1,48 @@
 import { View, Text ,StyleSheet, Image, Dimensions} from 'react-native'
 import React from 'react'
+import {LinearGradient} from "expo-linear-gradient";
+import Animated, { SharedValue, interpolate, useAnimatedStyle} from 'react-native-reanimated';
 
-const profile = {
-    id: 1,
-    image: "https://via.placeholder.com/300",
-    name: "Dani"
-}
+
 
 export const tinderCardWidth = Dimensions.get("screen").width *0.8;
 
-export default function TinderCard() {
+type TinderCard = {
+    user: {
+        image: string,
+        name: string,
+    };
+    numOfCards: number,
+    index: number,
+    activeIndex: SharedValue<number>,
+}
+
+export default function TinderCard({user,numOfCards,index,activeIndex}: TinderCard) {
+    
+    const style = useAnimatedStyle(() => ({
+        opacity: interpolate(activeIndex.value, [index-1, index, index+1],[1- 1/5,1,1]),
+    }));
+
   return (
-    <View style={styles.card}>
-        <Image style={[StyleSheet.absoluteFillObject,styles.image]} source={{uri: profile.image}} />
-      <Text style={styles.name}>{profile.name}</Text>
-    </View>
+    <Animated.View style={[styles.card, 
+    {
+        zIndex: numOfCards-index, 
+        opacity: 1-index * 0.2,
+        transform: [
+            {
+            translateY: -index*30},
+            {scale: 1- index * 0.05},
+            ]}]}>
+        <Image style={[StyleSheet.absoluteFillObject,styles.image]} source={{uri: user.image}} />
+
+        <LinearGradient 
+        colors={["transparent","rgba(0,0,0,0.8)"]}
+        style={[StyleSheet.absoluteFill,styles.overlay]}
+        />
+      <View style={styles.footer}>
+      <Text style={styles.name}>{user.name}</Text>
+      </View>
+    </Animated.View>
   )
 };
 
@@ -24,6 +52,8 @@ const styles = StyleSheet.create({
         // height: tinderCardWidth * 1.67,
         aspectRatio: 1/1.67,
         justifyContent: "flex-end",
+        position: "absolute",
+        //shadows
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -33,11 +63,20 @@ const styles = StyleSheet.create({
         shadowRadius: 2.22,
         elevation: 3,
     },
+    footer: {
+        padding: 10,
+    },
     image: {
         borderRadius: 15,
+    },
+    overlay: {
+        top: "50%",
+        borderBottomLeftRadius: 15,
+        borderBottomRightRadius: 15
     },
     name: {
         fontSize: 24,
         color: "white",
+        fontFamily: "InterBold",
     },
 });
