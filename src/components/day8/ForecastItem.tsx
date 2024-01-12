ec391b94607a415ee7dfaad25f0b57bf3ddd8769
Anıl <anil@Anl-MacBook-Air.local>
2024-01-12 20:34:09 +0300

    blurview

--- src/components/day8/ForecastItem.tsx
+++ src/components/day8/ForecastItem.tsx
@@ -2,35 +2,38 @@ import { View, Text ,StyleSheet} from 'react-native'
 import React from 'react'
 import { WeatherForecast } from '@/app/(days)/day8/weather'
 import dayjs from "dayjs";
+import {BlurView} from "expo-blur";
 
 export default function ForecastItem({forecast}:{forecast: WeatherForecast}) {
   return (
-    <View style={styles.container}>
+    <BlurView intensity={30} style={styles.container}>
       <Text style={styles.temperature}>{Math.round(forecast.main.temp)}°</Text>
       <Text style={styles.date}>{dayjs(forecast.dt*1000).format('ddd ha')}</Text>
-    </View>
+    </BlurView>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "ghostwhite",
         padding: 10,
         height: "100%",
         aspectRatio: 9/16,
         borderRadius: 10,
         alignItems: "center",
         justifyContent: "center",
+        overflow: 'hidden',
+        borderColor: "gainsboro",
+        borderWidth: StyleSheet.hairlineWidth,
     },
     temperature: {
         fontFamily: "InterBlack",
         fontSize: 35,
-        color: "grey",
+        color: "white",
         marginVertical: 10,
     },
     date: {
         fontFamily: "Inter",
-        color: "grey",
+        color: "ghostwhite",
         fontSize: 16,
     }
 })
